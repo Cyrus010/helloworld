0b93440f5ee794313ab0730a1fc6a40ad9883b22
Cyrus010 <89294697+Cyrus010@users.noreply.github.com>
2022-02-14 15:46:42 +0530

    Update Helloworld.java

--- src/main/java/helloworld/Helloworld.java
+++ src/main/java/helloworld/Helloworld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world !!!!");
+		System.out.println("hello world by varnit ");
 		// TODO Auto-generated method stub
 
 	}
